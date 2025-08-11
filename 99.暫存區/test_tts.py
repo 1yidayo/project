8a5adc1d8e97bbbd1be9e5676d4f4e758512cbe5
1yidayo <yi.412402232@gmail.com>
2025-08-11 16:31:18 +0800

    stt/tts

---
+++ 99.暫存區/test_tts.py
@@ -0,0 +1,27 @@
+from TTS.api import TTS #conda tts_env_310
+
+# 選一個支援多語系與多說話者的模型
+model_name = "tts_models/multilingual/multi-dataset/your_tts"
+
+# 載入模型
+tts = TTS(model_name)
+
+# 查看支援的語言與說話人（建議執行一次看看有哪些）
+print("支援的語言：", tts.languages)
+print("支援的說話人：", tts.speakers)
+
+# 指定語言與說話人（從上面列表中挑選）
+language = "en"         # 報錯無中文
+# speaker = "thorsten"    # 看你安裝的模型支援哪些，有可能要換其他的
+
+# 要轉成語音的文字
+# text = "你好，我是AI語音合成系統。"
+text = "Hello, i' m AI voice robot. Nice to meet you."
+
+# 輸出音檔路徑
+output_path = r"D:\Desktop\-\testtttt\tts_stt\output.wav"
+
+# 執行 TTS 並輸出檔案
+tts.tts_to_file(text=text, speaker=tts.speakers[0], language=language, file_path=output_path)
+
+print("語音已儲存到", output_path)
